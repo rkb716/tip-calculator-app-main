82d83ac6cfc9900b3cf009ca5f9ffffb9d73d556
Ryan Billings <ryankbillings@gmail.com>
2023-03-22 15:33:59 -0400

    Custom tip functionality

--- app.js
+++ app.js
@@ -12,13 +12,28 @@ const updateNumberOfPeople = () => {
     updateCalculationDisplay();
 }
 
+const updateCustomTip = () => {
+    let value = $('#tip-btn-6').val();
+    value = value.replace("%", "");
+    if(isNaN(value)){
+        value = 0
+    }
+    value = Number(value);
+    updateTip(Math.max(0, value / 100));
+    $('#tip-btn-6').val(value + "%");
+}
+
+const resetCustomTipButton = () => {
+    $('#tip-btn-6').val("");
+}
+
 const updateTip = (newTipAmount) => {
     tipAmount = newTipAmount;
     updateCalculationDisplay();
 }
 
 const updateSelectedButton = (index) => {
-    for(let i=0; i < 6; i++) {
+    for(let i=0; i < 7; i++) {
         let newClass = "unselected";
         if(index == i) {
             newClass = "selected";
@@ -28,6 +43,7 @@ const updateSelectedButton = (index) => {
 }
 
 const reset = () => {
+    resetCustomTipButton();
     currentBill = 0;
     tipAmount = .15;
     numberOfPeople = 1;
@@ -48,23 +64,32 @@ $(document).ready(() => {
     $(document).on("click", '#tip-btn-1', {"param": .05, "index": 1}, (event) => {
         updateTip(event.data.param);
         updateSelectedButton(event.data.index);
+        resetCustomTipButton();
     });
     $(document).on("click", '#tip-btn-2', {"param": .1, "index": 2}, (event) => {
         updateTip(event.data.param);
         updateSelectedButton(event.data.index);
+        resetCustomTipButton();
     });
     $(document).on("click", '#tip-btn-3', {"param": .15, "index": 3}, (event) => {
         updateTip(event.data.param);
         updateSelectedButton(event.data.index);
+        resetCustomTipButton();
     });
     $(document).on("click", '#tip-btn-4', {"param": .25, "index": 4}, (event) => {
         updateTip(event.data.param);
         updateSelectedButton(event.data.index);
+        resetCustomTipButton();
     });
     $(document).on("click", '#tip-btn-5', {"param": .5, "index": 5}, (event) => {
         updateTip(event.data.param);
         updateSelectedButton(event.data.index);
+        resetCustomTipButton();
     });
+    $(document).on("click", '#tip-btn-6', {"index": 6}, (event) => {
+        updateSelectedButton(event.data.index);
+    });
+    $(document).on("input", "#tip-btn-6", updateCustomTip);
     $(document).on("click", "#reset-btn", reset);
     $(document).on("input", "#bill-input", updateBill);
     $(document).on("input", "#people-input", updateNumberOfPeople);
